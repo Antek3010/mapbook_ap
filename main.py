8da87637a7d5a18a3cd2fec488fd82b7f769a4f4
Antek3010 <antek297@wp.pl>
2025-04-08 19:01:01 +0200

    chore: mvt pattern

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-users=[
-    {"name":"Antek","location":"Zwoleń","posts":500},
-    {"name":"Michał","location":"Krasnystaw","posts":200},
-    {"name":"Ksavier","location":"Grudziąc","posts":100},
-    {"name":"Damian","location":"Kraków","posts":300},
+from utils.controller import get_user_info
+from utils.model import users
 
-]
+def main():
+    get_user_info(users)
 
 
 
-def get_user_info(users_data: list)->None:
-    for user in users_data:
-        print(f"Twój znajomy {user['name']}, z miejscowości: {user['location']} opublikował {user['posts']} postów")
-
-get_user_info(users)
+if __name__ == '__main__':
+    main()
