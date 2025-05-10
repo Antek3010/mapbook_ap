d52e2aa8139653d07e3e8548ae3f515e38ae955c
Antek3010 <antek297@wp.pl>
2025-05-11 01:10:46 +0200

    wip: prepared add_user

--- utils/notatnik.py
+++ utils/notatnik.py
@@ -1,10 +1,12 @@
-def get_user_info(users_data: list)->None:
-    for user in users_data:
-        print(f"Twój znajomy {user['name']}, z miejscowości: {user['location']} opublikował {user['posts']} postów")
-
+moja_lista_na_sok=[]
+print(moja_lista_na_sok)
 
 def add_user(users_data: list)-> None:
+
     new_name=input('podaj imię nowego użytkownika: ')
     new_location=input('podaj lokalizację nowego użytkownika: ')
     new_posts=int(input('podaj liczbę postów nowego użytkownika '))
     users_data.append({"name": new_name , "location": new_location,"posts": new_posts})
+
+add_user(moja_lista_na_sok)
+print(moja_lista_na_sok)
