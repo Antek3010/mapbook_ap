9caad9ee1895754e0ec724381f7057233ee75d98
Antek3010 <antek297@wp.pl>
2025-05-11 20:08:05 +0200

    wip: prepared map

--- utils/model.py
+++ utils/model.py
@@ -1,7 +1,7 @@
 users=[
-    {"name":"Antek","location":"Zwoleń","posts":500},
+    {"name":"Antek","location":"Lublin","posts":500},
     {"name":"Michał","location":"Krasnystaw","posts":200},
-    {"name":"Ksavier","location":"Grudziąc","posts":100},
+    {"name":"Ksavier","location":"Wrocław","posts":100},
     {"name":"Damian","location":"Kraków","posts":300},
 
 ]
