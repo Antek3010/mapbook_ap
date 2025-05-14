97d8ced3ac5167c589c1dae20195408f178bd382
Antek3010 <antek297@wp.pl>
2025-05-14 18:12:58 +0200

    wip: finish

--- notatnik.py
+++ notatnik.py
@@ -1,37 +1,27 @@
-users=[
-    {"name":"Antek","location":"Lublin","posts":500,},
-    {"name":"Michał","location":"Krasnystaw","posts":200,},
-    {"name":"Ksavier","location":"Wrocław","posts":100,},
-    {"name":"Damian","location":"Kraków","posts":300,},
+class User:
+    def __init__(self, name:str, surname:str, location:str, posts: str):
+        self.name =name
+        self.surname = surname
+        self.location = location
+        self.posts = posts
+        self.coordinates=self.get_coordinates()
 
-]
+    def get_coordinates(self) -> list:
+        import requests
+        from bs4 import BeautifulSoup
 
-import folium
-
-import requests
-from bs4 import BeautifulSoup
-
-#https://pl.wikipedia.org/wiki/Przybys%C5%82awice_(wojew%C3%B3dztwo_lubelskie)
-
-def get_coordinates(city:str)->list:
-
-    url=f'https://pl.wikipedia.org/wiki/{city}'
-    response = requests.get(url).text
-    response_html = BeautifulSoup(response, 'html.parser')
-    longitude=float(response_html.select('.longitude')[1].text. replace(',','.'))
-    latitude=float(response_html.select('.latitude')[1].text. replace(',','.'))
-    return [latitude,longitude]
+        url = f'https://pl.wikipedia.org/wiki/{self.location}'
+        response = requests.get(url).text
+        response_html = BeautifulSoup(response, 'html.parser')
+        longitude = float(response_html.select('.longitude')[1].text.replace(',', '.'))
+        latitude = float(response_html.select('.latitude')[1].text.replace(',', '.'))
+        return [latitude, longitude]
 
 
-def get_map(users_data:list)->None:
-    map = folium.Map(location=(52.23, 21.00), zoom_start=6)
-    for user in users_data:
-        coordinates = get_coordinates(user['location'])
 
-        folium.Marker(
-            location=(coordinates[0],coordinates[1]),
-            popup=f"Twój znajomy {user['name']}, <br/> miejscowość: {user['location']} <br/> opublikował {user['posts']} postów").add_to(
-            map)
-    map.save('mapa.html')
 
-get_map(users)
+
+user_1=User(name='aaa', surname='bbb', location='Warszawa', posts='zzz')
+print(user_1.name, user_1.surname, user_1.location, user_1.posts, user_1.coordinates)
+user_2=User(name='ccc', surname='fff', location='Kraków', posts='zvvzz')
+print(user_2.name, user_2.surname, user_2.location, user_2.posts, user_2.coordinates)
